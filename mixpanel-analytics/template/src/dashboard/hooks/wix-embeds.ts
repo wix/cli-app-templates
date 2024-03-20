62df76d3e885d08c0da08e7fe6c9cb830d9a85e0
Omri Maman <maomri1@gmail.com>
2024-03-20 10:44:40 +0200

    change embedded script API to be called using app market SDK

--- mixpanel-analytics/template/src/dashboard/hooks/wix-embeds.ts
+++ mixpanel-analytics/template/src/dashboard/hooks/wix-embeds.ts
@@ -1,41 +1,29 @@
-import { useWixFetch } from '@wix/sdk-react';
+import { useWixModules } from '@wix/sdk-react';
+import { embeddedScripts } from '@wix/app-market';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-
-// https://dev.wix.com/docs/rest/api-reference/app-management/apps/embedded-scripts/introduction
-const EMBEDDED_SCRIPT_API_URL = 'https://www.wixapis.com/apps/v1/scripts';
 
 export const QUERY_EMBEDS = 'queryEmbeds';
 export const MUTATE_EMBEDS = 'mutateEmbeds';
 
-export const useEmbeds = <T>() => {
-  const wixFetch = useWixFetch();
+export const useEmbeds = <T extends Record<string, string>>() => {
+  const embeddedScriptsModule = useWixModules(embeddedScripts);
   const queryClient = useQueryClient();
 
-  const queryEmbeds = useQuery<unknown, unknown, T>({
+  const getEmbeddedScript = useQuery<unknown, unknown, T>({
     queryKey: [QUERY_EMBEDS],
     refetchOnWindowFocus: false,
     queryFn: async () => {
-      const data = (await wixFetch(EMBEDDED_SCRIPT_API_URL, {
-        method: 'get',
-        headers: { 'content-type': 'application/json' },
-      }).then((res) => res.json())) as { properties: { parameters: T } };
-
-      return data?.properties?.parameters;
+      // https://dev.wix.com/docs/sdk/backend-modules/app-market/embedded-scripts/get-embedded-script
+      const embeddedScript = await embeddedScriptsModule.getEmbeddedScript();
+      return embeddedScript.parameters || {};
     },
   });
 
-  const injectEmbeds = useMutation<unknown, unknown, T>({
+  const embedScript = useMutation<unknown, unknown, T>({
     mutationKey: [MUTATE_EMBEDS],
     mutationFn: async (parameters) => {
-      await wixFetch(EMBEDDED_SCRIPT_API_URL, {
-        method: 'post',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({
-          properties: {
-            parameters,
-          },
-        }),
-      });
+      // https://dev.wix.com/docs/sdk/backend-modules/app-market/embedded-scripts/embed-script
+      await embeddedScriptsModule.embedScript({ parameters });
       return parameters;
     },
     onSuccess: (data) => {
@@ -43,5 +31,5 @@ export const useEmbeds = <T>() => {
     },
   });
 
-  return { queryEmbeds, injectEmbeds };
+  return { embedScript, getEmbeddedScript };
 };
