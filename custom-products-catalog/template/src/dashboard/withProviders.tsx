a7aedf7cc57717d59466427365e7f161cbe2e255
Adir Amsalem <adira@wix.com>
2024-09-02 14:53:34 +0300

    custom-products-catalog: remove `locale`

--- custom-products-catalog/template/src/dashboard/withProviders.tsx
+++ custom-products-catalog/template/src/dashboard/withProviders.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { WixDesignSystemProvider } from '@wix/design-system';
-import { withDashboard, EnvironmentState} from '@wix/dashboard-react';
+import { withDashboard } from '@wix/dashboard-react';
 import { WixPatternsProvider } from '@wix/patterns/provider';
-import { useEnvironment } from '@wix/sdk-react';
 
 export function withProviders<P extends {} = {}>(Component: React.FC<P>) {
   return withDashboard(function DashboardProviders(props: P) {
-    const { language } = useEnvironment<EnvironmentState>();
     return (
-      <WixDesignSystemProvider locale={language} features={{ newColorsBranding: true }}>
+      <WixDesignSystemProvider features={{ newColorsBranding: true }}>
         <WixPatternsProvider>
           <Component {...props} />
         </WixPatternsProvider>
