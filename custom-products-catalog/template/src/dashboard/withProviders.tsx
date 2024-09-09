cdbad6c32333093f18e62267ea57a94e1d47fa03
Adir Amsalem <adira@wix.com>
2024-09-09 15:23:17 +0300

    add withDashboard

--- custom-products-catalog/template/src/dashboard/withProviders.tsx
+++ custom-products-catalog/template/src/dashboard/withProviders.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { WixDesignSystemProvider } from '@wix/design-system';
+import { withDashboard } from '@wix/dashboard-react';
 import { WixPatternsProvider } from '@wix/patterns/provider';
 import { i18n } from '@wix/essentials';
 
 export function withProviders<P extends {} = {}>(Component: React.FC<P>) {
-  return function DashboardProviders(props: P) {
+  return withDashboard(function DashboardProviders(props: P) {
     const locale = i18n.getLocale();
     return (
       <WixDesignSystemProvider locale={locale} features={{ newColorsBranding: true }}>
@@ -13,5 +14,5 @@ export function withProviders<P extends {} = {}>(Component: React.FC<P>) {
         </WixPatternsProvider>
       </WixDesignSystemProvider>
     );
-  };
+  });
 }
