607ccd33797a56b373ca61d572983dee62f99bb9
omri maman <maomri1@gmail.com>
2024-09-17 10:50:03 +0300

    Update wix-embeds docs links

--- site-popup/template/src/dashboard/hooks/wix-embeds.ts
+++ site-popup/template/src/dashboard/hooks/wix-embeds.ts
@@ -11,7 +11,7 @@ export const useEmbeds = <T extends Record<string, string>>() => {
     queryKey: [QUERY_EMBEDS],
     refetchOnWindowFocus: false,
     queryFn: async () => {
-      // https://dev.wix.com/docs/sdk/backend-modules/app-market/embedded-scripts/get-embedded-script
+      // https://dev.wix.com/docs/sdk/backend-modules/app-management/embedded-scripts/get-embedded-script
       const embeddedScript = await embeddedScripts.getEmbeddedScript();
       return embeddedScript.parameters || {};
     },
@@ -20,7 +20,7 @@ export const useEmbeds = <T extends Record<string, string>>() => {
   const embedScript = useMutation<unknown, unknown, T>({
     mutationKey: [MUTATE_EMBEDS],
     mutationFn: async (parameters) => {
-      // https://dev.wix.com/docs/sdk/backend-modules/app-market/embedded-scripts/embed-script
+      // https://dev.wix.com/docs/sdk/backend-modules/app-management/embedded-scripts/embed-script
       await embeddedScripts.embedScript({ parameters });
       return parameters;
     },
