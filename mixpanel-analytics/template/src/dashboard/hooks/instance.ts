0e3d781c01fee3617e96dd4214c57090958cb136
Denys Dovhan <denysdovhan@gmail.com>
2024-11-15 19:53:50 +0200

    docs: add comment about pricing page

--- mixpanel-analytics/template/src/dashboard/hooks/instance.ts
+++ mixpanel-analytics/template/src/dashboard/hooks/instance.ts
@@ -1,8 +1,14 @@
 import { useCallback, useEffect, useState } from 'react';
 import { appInstances } from '@wix/app-management';
 
-// FIXME: Pass APPID when app is generated
-// const PRICING_PAGE_URL = 'https://www.wix.com/apps/upgrade/APPIDHERE?appInstanceId=INSTANCEIDHERE'
+/*
+  This is the URL to the pricing page of the app.
+  This url looks like this:
+    https://www.wix.com/apps/upgrade/APPIDHERE?appInstanceId=INSTANCEIDHERE
+ 
+  You can find more information about this here:
+    https://dev.wix.com/docs/build-apps/launch-your-app/pricing-and-billing/set-up-a-freemium-business-model#step-4--create-an-upgrade-entry-point-to-your-pricing-page
+*/
 const getPricingPage = (instanceId: string) => `https://www.wix.com/apps/upgrade/<%= devCenter.appId %>?appInstanceId=${instanceId}`
 
 export function useAppInstance() {
