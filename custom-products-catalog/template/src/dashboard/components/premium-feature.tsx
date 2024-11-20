e8536fe57c9b539d2595dec641dfc1ca55c61955
Denys Dovhan <denysdovhan@gmail.com>
2024-11-20 15:49:28 +0200

    style: fix indentation for premium feature

--- custom-products-catalog/template/src/dashboard/components/premium-feature.tsx
+++ custom-products-catalog/template/src/dashboard/components/premium-feature.tsx
@@ -3,33 +3,33 @@ import { FloatingHelper } from '@wix/design-system'
 import { useFreeTrialAvailable, useIsFree, useNavigateToPricingPage } from '../hooks/instance';
 
 interface PremiumFeatureProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function PremiumFeature({ children }: PremiumFeatureProps) {
-    const isFree = useIsFree();
-    const isFreeTrialAvailable = useFreeTrialAvailable();
-    const navigateToPricingPage = useNavigateToPricingPage();
+  const isFree = useIsFree();
+  const isFreeTrialAvailable = useFreeTrialAvailable();
+  const navigateToPricingPage = useNavigateToPricingPage();
 
-    if (isFree && isFreeTrialAvailable) {
-      return (
-        <FloatingHelper
-          placement="bottom-end"
-          appearance="light"
-          initiallyOpened={false}
-          target={children}
-          content={
-            <FloatingHelper.Content
-              title="Start your free trial"
-              body="You need a premium subscription to access this feature. Start your free trial now."
-              actionText="Start Free Trial"
-              actionTheme="premium"
-              onActionClick={navigateToPricingPage}
-            />
-          }
-        />
-      );
-    }
+  if (isFree && isFreeTrialAvailable) {
+    return (
+      <FloatingHelper
+        placement="bottom-end"
+        appearance="light"
+        initiallyOpened={false}
+        target={children}
+        content={
+          <FloatingHelper.Content
+            title="Start your free trial"
+            body="You need a premium subscription to access this feature. Start your free trial now."
+            actionText="Start Free Trial"
+            actionTheme="premium"
+            onActionClick={navigateToPricingPage}
+          />
+        }
+      />
+    );
+  }
 
-    return <>{children}</>;
+  return <>{children}</>;
 }
