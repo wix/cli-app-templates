ca03a906053716e12a5f29fea7d8e4881c1b3269
Tomer Kruvi <tomerk@wix.com>
2025-04-15 15:53:08 +0300

    Refactor

--- tests/testkit.ts
+++ tests/testkit.ts
@@ -1,27 +1,32 @@
-import { spawn, SpawnOptions } from "child_process";
-import { mkdir, rm } from "fs";
-import { promisify } from "util";
+import { spawn, SpawnOptions } from "node:child_process";
+import { mkdir, rm } from "node:fs";
+import { promisify } from "node:util";
+import { tmpdir } from 'node:os';
+import path from 'node:path';
+import { C } from "vitest/dist/chunks/reporters.d.CfRkRKN2";
 
 const mkdirAsync = promisify(mkdir);
 const rmAsync = promisify(rm);
 
 const CACHE_DIR = '.cache';
 
+const templateDir = (template: string) => path.join(CACHE_DIR, template);
+
 const kebabToCamelCase = (str: string) =>
     str.replace(/-./g, match => match[1].toUpperCase());
 
 const silentSpawn = (command: string, args: string[], options?: SpawnOptions) =>
     new Promise<void>((resolve, reject) => {
-        const proc = spawn(command, args, { stdio: 'ignore', ...options });
+        const process = spawn(command, args, { stdio: 'ignore', ...options });
 
-        proc.on('error', reject);
-        proc.on('close', (code) => {
+        process.on('error', reject);
+        process.on('close', (code) => {
             code === 0 ? resolve() : reject(new Error(`CLI exited with code ${code} `));
         });
     });
 
 export const createApp = async (template: string) => {
-    await mkdirAsync(CACHE_DIR);
+    await mkdirAsync(CACHE_DIR, { recursive: true });
     await silentSpawn('yarn',
         [
             'create',
@@ -35,10 +40,10 @@ export const createApp = async (template: string) => {
 }
 
 export const installDependencies = async (template: string) =>
-    await silentSpawn('yarn', ['install', '--silent', '--prefer-offline'], { 'cwd': `${CACHE_DIR}/${template}` })
+    await silentSpawn('yarn', ['install', '--silent', '--prefer-offline'], { 'cwd': templateDir(template) })
 
 export const buildApp = async (template: string) =>
-    await silentSpawn('yarn', ['build'], { 'cwd': `${CACHE_DIR}/${template}` })
+    await silentSpawn('yarn', ['build'], { 'cwd': templateDir(template) })
 
 export const clearCache = () => rmAsync(CACHE_DIR, { recursive: true, force: true });
 
