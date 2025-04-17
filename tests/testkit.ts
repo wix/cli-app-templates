32e45c1f8a8fdb0feb43ca1d5d59cd126a5c8079
Tomer Kruvi <tomerk@wix.com>
2025-04-17 12:58:35 +0300

    wip

--- tests/testkit.ts
+++ tests/testkit.ts
@@ -4,51 +4,50 @@ import { fileURLToPath } from 'node:url';
 import { temporaryDirectory } from 'tempy';
 
 const kebabToCamelCase = (str: string) =>
-    str.replace(/-./g, match => match[1].toUpperCase());
+  str.replace(/-./g, match => match[1].toUpperCase());
 
 const runCommand = async (command: string, args: string[], options?: Options) => {
-    try {
-        const { stdout } = await execa(command, args, { stdio: 'pipe', ...options });
-        return stdout;
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error((error as any).stderr || error.message);
-        }
-        throw error;
+  try {
+    const { stdout } = await execa(command, args, { stdio: 'pipe', ...options });
+    return stdout;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error((error as any).stderr || error.message);
     }
+    throw error;
+  }
 };
 
 export const createApp = async (template: string) => {
-    try {
-        const cwd = temporaryDirectory();
-        const dirname = path.dirname(fileURLToPath(import.meta.url));
-        const templatePath = path.join(dirname, `../${template}/template`);
-        await runCommand('yarn',
-            [
-                'create',
-                '@wix/app',
-                '--app-name',
-                kebabToCamelCase(template),
-                '--template-path',
-                templatePath,
-                '--skip-install',
-                '--skip-git'
-            ], { cwd, }
-        )
-        return path.join(cwd, template);
-    } catch (e) {
-        console.error(e);
-        throw new Error(`Failed to create app from template "${template}".\n${e}`);
-    }
+  try {
+    const cwd = temporaryDirectory();
+    const dirname = path.dirname(fileURLToPath(import.meta.url));
+    const templatePath = path.join(dirname, `../${template}/template`);
+    await runCommand('yarn',
+      [
+        'create',
+        '@wix/app',
+        '--app-name',
+        kebabToCamelCase(template),
+        '--template-path',
+        templatePath,
+        '--skip-install'
+      ], { cwd, }
+    )
+    return path.join(cwd, template);
+  } catch (e) {
+    console.error(e);
+    throw new Error(`Failed to create app from template "${template}".\n${e}`);
+  }
 }
 
 export const installDependencies = async (cwd: string) =>
-    await runCommand('yarn', ['install', '--silent'], { cwd, env: { YARN_ENABLE_IMMUTABLE_INSTALLS: 'false' } })
+  await runCommand('yarn', ['install', '--silent'], { cwd, env: { YARN_ENABLE_IMMUTABLE_INSTALLS: 'false' } })
 
 export const checkTypes = async (cwd: string) =>
-    await runCommand('yarn', ['typecheck'], { cwd })
+  await runCommand('yarn', ['typecheck'], { cwd })
 
 export const buildApp = async (cwd: string) =>
-    await runCommand('yarn', ['build'], { cwd })
+  await runCommand('yarn', ['build'], { cwd })
 
 
