17c9efcc942ccc4a530b1d7a7d4edbf03b6ee6b2
Artem Demochko <artemdem@wix.com>
2025-10-30 16:53:56 +0200

    upd astro.config

--- astro/app/blank/astro.config.mjs
+++ astro/app/blank/astro.config.mjs
@@ -2,7 +2,22 @@
 import { defineConfig } from 'astro/config';
 import wix from '@wix/astro';
 
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [wix()],
+  integrations: [wix(), react()],
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: false,
+    },
+  }),
+
+  image: {
+    domains: ["static.wixstatic.com"],
+  },
+
+  output: "server",
 });
