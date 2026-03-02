b4f4b6dbccef0df6dea11a95c3d77756312bfa38
Mantas Miliukas <mantasm@wix.com>
2026-03-02 11:13:56 +0200

    chore(@wix/dashboard-react): cleanup

--- custom-products-catalog/template/src/extensions/dashboard/withProviders.tsx
+++ custom-products-catalog/template/src/extensions/dashboard/withProviders.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { WixDesignSystemProvider } from '@wix/design-system';
-import { withDashboard } from '@wix/dashboard-react';
 import { WixPatternsProvider } from '@wix/patterns/provider';
 import { i18n } from '@wix/essentials';
 
 export function withProviders<P extends {} = {}>(Component: React.FC<P>) {
-  return withDashboard(function DashboardProviders(props: P) {
+  return function DashboardProviders(props: P) {
     const locale = i18n.getLocale();
     return (
       <WixDesignSystemProvider locale={locale} features={{ newColorsBranding: true }}>
@@ -14,5 +13,5 @@ export function withProviders<P extends {} = {}>(Component: React.FC<P>) {
         </WixPatternsProvider>
       </WixDesignSystemProvider>
     );
-  });
+  };
 }
